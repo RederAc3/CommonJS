5b8afab4288a9e26b459e64407c45089b187b8f4
Andrzej Kot <reder.ac3@gmail.com>
2019-03-18 22:13:05 +0100

    Add color text

--- modules/forTime.js
+++ modules/forTime.js
@@ -1,16 +1,17 @@
 var os = require('os');
+var colors = require('colors');
 
 function formatTime() {
     var timeSec = os.uptime();
     var timeMin = Math.floor((timeSec / 60));
     var timeHour = Math.floor((timeMin / 60));
-
+    var Uptime = 'Uptime:'.green;
     if (timeSec > 60 && timeSec < 3600) {
-        console.log('Uptime:', timeMin, 'min', timeSec - (timeMin * 60), 'sec');
+        console.log(Uptime, timeMin, 'min', timeSec - (timeMin * 60), 'sec');
     } else if (timeSec > 3600) {
-        console.log('Uptime:', timeHour, 'godz', timeMin - (timeHour * 60), 'min', timeSec - (timeMin * 60), 'sec');
+        console.log(Uptime, timeHour, 'godz', timeMin - (timeHour * 60), 'min', timeSec - (timeMin * 60), 'sec');
     } else if (timeSec < 60) {
-        console.log('Uptime:', timeSec, 'sec');
+        console.log(Uptime, timeSec, 'sec');
     }
 }
 
